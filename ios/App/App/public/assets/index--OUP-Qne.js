32a8ccb9d1ff58ab7f452b1fedc01e77355f3171
Louise Bencard <louisebencard@Louises-MacBook-Air-2.local>
2026-05-05 13:47:48 +0200

    chore: opdater iOS public assets efter cap sync

---
+++ ios/App/App/public/assets/index--OUP-Qne.js
@@ -0,0 +1,1 @@
+import{r as i}from"./index-j0KkaPV9.js";const t=i("PushNotifications",{});export{t as PushNotifications};
--- ios/App/App/public/assets/index-DYORcGjY.js
+++
@@ -1,1 +0,0 @@
-import{r as i}from"./index-BU630B_N.js";const t=i("PushNotifications",{});export{t as PushNotifications};
