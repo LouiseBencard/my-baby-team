85eb6d592923022f698551d09bca0b564993d0c4
Louise Bencard <louisebencard@Louises-MacBook-Air-2.local>
2026-05-07 05:57:22 +0200

    Fix: commit updated iOS web assets (ios/App/App/public)

--- ios/App/App/public/assets/index--OUP-Qne.js
+++
@@ -1,1 +0,0 @@
-import{r as i}from"./index-j0KkaPV9.js";const t=i("PushNotifications",{});export{t as PushNotifications};
---
+++ ios/App/App/public/assets/index-BoFYCrWa.js
@@ -0,0 +1,1 @@
+import{r as i}from"./index-D69haucO.js";const t=i("PushNotifications",{});export{t as PushNotifications};
